bb30e6cf463f79facaa22994cee409ffa40cd207
alvega-g <alvarg@gmail.com>
2023-11-16 16:03:49 +0100

    map checking started

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:34:57 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/11/16 13:09:56 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/11/16 14:00:52 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int add_to_map(data_t *game, char *line)
+static int add_to_map(data_t *game, char *line)
 {	
 	char **temp;
 	int i;
@@ -28,9 +28,21 @@ int add_to_map(data_t *game, char *line)
 		temp[i] = game->map[i];
 		i++;
 	}
+	if (game->map)
+		free(game->map);
 	temp[i] = line;
 	game->map = temp;
 	return (1);
+}
+
+static int map_width(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i])
+		i++;
+	return (i);
 }
 
 void populate_map(data_t *game, char *av)
@@ -46,10 +58,46 @@ void populate_map(data_t *game, char *av)
 			break ;
 	}
 	close(game->fd);
-	for (int i = 0; i < game->map_height; i++)
-		ft_printf("%s", game->map[i]);
-
+	game->map_width = map_width(game->map[0]);
 }
+
+/*
+	Does the map have walls (1) all around it
+	Does the map only have one exit (E), one starting position (P) and at least one collectable (C)
+	Is there a valid map from P to E collecting all C
+*/
+
+int map_check(char **map, int len)
+{
+	int i;
+	
+	int player_flag;
+	int exit_flag;
+	
+	i = 0;
+	while (map[i])
+	{
+		if (ft_strlen(map[i]) != len) // Inconsistent width
+			return (0);
+		if (!ft_strchr(map[i], "P")) // No initial position
+			return (0);
+		if (ft_strchr(map[i], "P") && player_flag == 1) // More than one starting position
+			return (0);
+		if (!ft_strchr(map[i], "C")) // No collectable
+			return (0);
+		if (!ft_strchr(map[i], "E" && exit_flag == 1)) // No exit
+			return (0);
+		i++;
+	}
+	return (1);
+}
+int is_map_correct(data_t game)
+{
+	if (!map_check(game->map, game->map_width));
+		return (0);
+	
+	
+} 
 
 int main(int ac, char **av)
 {
@@ -59,5 +107,7 @@ int main(int ac, char **av)
 	if (ac == 2)
 	{
 		populate_map(&game, av[1]);
+		if (!is_map_correct(&game))
+			return (0);
 	}
 }
